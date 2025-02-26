08766b01e4941db2b58b4c7ca15fca421a71033e
Lars Ludvigsen <larsll@outlook.com>
2025-02-26 07:46:09 +0000

    More control handling

--- build_scripts/files/common/nginx_install_certs.sh
+++ build_scripts/files/common/nginx_install_certs.sh
@@ -26,17 +26,26 @@ convert_subject_format() {
     echo "$subject" | sed -e 's/subject=/\//' -e 's/, /\//g' -e 's/ = /=/g'
 }
 
+
+# Check if the default password file exists
+if [ ! -f /opt/aws/deepracer/password.txt ]; then
+    # Creating the webserver default username and password (deepracer)
+    python3 $ROOT_DIR/reset_default_password.py
+else
+    echo "Password file already exists. Skipping password creation."
+fi
+
 # Check if the certificate and key exist
 if [ -f "$CERT_FILE" ] && [ -f "$KEY_FILE" ]; then
     # Check if the certificate and key match
-    if [ "$(openssl x509 -noout -modulus -in "$CERT_FILE" | openssl md5)" == "$(openssl rsa -noout -modulus -in "$KEY_FILE" | openssl md5)" ]
+    if [ "$(openssl x509 -noout -modulus -in "$CERT_FILE" | openssl md5)" == "$(openssl rsa -noout -modulus -in "$KEY_FILE" | openssl md5)" ]; then
         # Check if the certificate is issued by the required subject
         cert_subject=$(openssl x509 -in "$CERT_FILE" -noout -subject)
         normalized_subject=$(convert_subject_format "$cert_subject")
         if [ "$normalized_subject" == "$SUBJECT" ]; then
             # Check if the certificate is valid for at least 365 days
             if is_cert_valid_for_365_days "$CERT_FILE"; then
-                echo "Existing certificate is valid and matches the required criteria. Skipping certificate creation."
+                echo "Existing certificate is valid. Skipping certificate creation."
                 exit 0
             fi
         fi
@@ -45,9 +54,3 @@ fi
 
 # Creating the Nginx certificates and placing to appropriate directory
 openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -keyout "$KEY_FILE" -out "$CERT_FILE" -subj "$SUBJECT"
-
-# Check if the default password file exists
-if [ ! -f /opt/aws/deepracer/password.txt ]; then
-    # Creating the webserver default username and password (deepracer)
-    python3 $ROOT_DIR/reset_default_password.py
-fi
