1be96dfc99f0554f0f6e1cac929f5474950f9a85
Lars Ludvigsen <larsll@outlook.com>
2025-06-08 18:11:42 +0200

    Add uvcvideo support

--- build_scripts/build-libcamera.sh
+++ build_scripts/build-libcamera.sh
@@ -49,7 +49,7 @@ else
 fi
 cd $DIR/deps/libcamera
 
-meson setup build --wipe --buildtype=release -Dpipelines=rpi/pisp -Dipas=rpi/pisp -Dv4l2=enabled -Dgstreamer=disabled -Dtest=false -Dlc-compliance=disabled -Dcam=enabled -Dqcam=disabled -Ddocumentation=disabled -Dpycamera=enabled --prefix=/opt/ros/$ROS_DISTRO
+meson setup build --wipe --buildtype=release -Dpipelines=uvcvideo,rpi/pisp -Dipas=rpi/pisp -Dv4l2=enabled -Dgstreamer=disabled -Dtest=false -Dlc-compliance=disabled -Dcam=enabled -Dqcam=disabled -Ddocumentation=disabled -Dpycamera=enabled --prefix=/opt/ros/$ROS_DISTRO
 DESTDIR=${DIR}/deps/libcamera-build ninja -C build install
 mkdir -p ${DESTDIR}/opt/ros/$ROS_DISTRO/lib/python3.12/site-packages
 mv ${DESTDIR}/opt/ros/$ROS_DISTRO/lib/aarch64-linux-gnu/python3.12/site-packages/libcamera ${DESTDIR}/opt/ros/$ROS_DISTRO/lib/python3.12/site-packages/libcamera
