c553b7d72e7f12bfb2e55c6af651a6bd935fa19e
Lars Ludvigsen <larsll@outlook.com>
2025-07-06 13:40:58 +0200

    Don't remap services

--- src/aws-deepracer-launcher/deepracer_launcher/launch/deepracer_launcher.py
+++ src/aws-deepracer-launcher/deepracer_launcher/launch/deepracer_launcher.py
@@ -14,10 +14,8 @@
 #   limitations under the License.                                              #
 #################################################################################
 
-from calendar import c
 import math
 
-from numpy import e
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, OpaqueFunction
 from launch.substitutions import LaunchConfiguration
@@ -96,14 +94,7 @@ def launch_setup(context, *args, **kwargs):
                 # Topic remappings
                 ('/camera_pkg/camera/camera_info', '/camera_pkg/camera_info'),
                 ('/camera_pkg/camera/image_raw', '/camera_pkg/display_mjpeg'),
-                ('/camera_pkg/camera/image_raw/compressed', '/camera_pkg/display_mjpeg/compressed'),
-                # Service remappings
-                ('/camera_pkg/camera/describe_parameters', '/camera_pkg/describe_parameters'),
-                ('/camera_pkg/camera/get_parameter_types', '/camera_pkg/get_parameter_types'),
-                ('/camera_pkg/camera/get_parameters', '/camera_pkg/get_parameters'),
-                ('/camera_pkg/camera/list_parameters', '/camera_pkg/list_parameters'),
-                ('/camera_pkg/camera/set_parameters', '/camera_pkg/set_parameters'),
-                ('/camera_pkg/camera/set_parameters_atomically', '/camera_pkg/set_parameters_atomically')
+                ('/camera_pkg/camera/image_raw/compressed', '/camera_pkg/display_mjpeg/compressed')
             ]
         )
 
