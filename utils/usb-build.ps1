c2e324766dc17ea9ae6fcf30376dafa557f0fb97
Lars Ludvigsen <larsll@outlook.com>
2026-01-25 20:17:52 +0100

    Continue parsing

--- utils/usb-build.ps1
+++ utils/usb-build.ps1
@@ -133,6 +133,7 @@ Function New-File-Download {
                 Start-BitsTransfer -Source $url.AbsoluteUri -Destination $FilePath -DisplayName "Downloading $(Split-Path $FilePath -Leaf)"
             } else {
                 Write-Host "  Download-File - Using Invoke-WebRequest method"
+                $ProgressPreference = 'Continue'
                 Invoke-WebRequest -Uri $url.AbsoluteUri -OutFile $FilePath -UseBasicParsing
             }
         } catch {
